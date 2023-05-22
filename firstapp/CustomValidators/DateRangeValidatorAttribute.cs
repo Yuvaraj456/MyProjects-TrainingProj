aa106964e3d4f128b28ad0b86ae0763aa6a22cb9
Yuvaraj Nagarajan <yuvarajryd1@outlook.com>
2023-05-22 11:18:41 +0530

    22/05/2023 Learnings

---
+++ firstapp/CustomValidators/DateRangeValidatorAttribute.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+namespace MyFirstApp.CustomValidators
+{
+    public class DateRangeValidatorAttribute : ValidationAttribute
+    {
+        public string OtherPropertyName { get; set; }
+
+
+        public DateRangeValidatorAttribute(string otherpropertyname) 
+        {
+            OtherPropertyName = otherpropertyname;
+
+        }
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if(value != null)
+            {
+                //get todate
+                DateTime to_date = Convert.ToDateTime(value);
+
+                //get from date
+                PropertyInfo? otherProperty = validationContext.ObjectType.GetProperty(OtherPropertyName);
+
+                if(otherProperty != null)
+                {
+                    DateTime from_date = Convert.ToDateTime(otherProperty?.GetValue(validationContext.ObjectInstance)); ;
+
+                    if (from_date > to_date)
+                    {
+                        return new ValidationResult(ErrorMessage, new string[] { validationContext.MemberName! });
+                    }
+                    else
+                    {
+                        return ValidationResult.Success;
+                    }
+
+                }
+               
+            }
+            return null;
+        }
+    }
+}
--- firstapp/Models/Person.cs
+++ firstapp/Models/Person.cs
@@ -1,15 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using MyFirstApp.CustomValidators;
 using System.ComponentModel.DataAnnotations;
 
 namespace MyFirstApp.Models
 {
-    public class Person
+    public class Person : IValidatableObject
     {
 
         [Required(ErrorMessage ="Please enter {0}")]
         [Display(Name ="Person Name")]
         [StringLength(10,MinimumLength =5,ErrorMessage ="{0} characters Length between {2} to {1}")]
         public string? PersonName { get; set; }
+
+        public int? Age { get; set; }
 
         public string? Address { get; set; }
 
@@ -20,19 +23,34 @@ namespace MyFirstApp.Models
         [EmailAddress(ErrorMessage ="Please enter valid {0}")]
         public string? EmailId { get; set;}
 
-        [Phone(ErrorMessage ="Please enter valid {0}")]
+        [Phone(ErrorMessage = "Please enter valid {0}")]
         public string? PhoneNumber { get; set;}
 
+        //[MinimumYearValidatorAttribute(2005, ErrorMessage = "Minimum Year required is above {0}")]
+        public DateTime DateOfBirth { get; set; }
 
         [RegularExpression("^[0-9]{6}$",ErrorMessage ="Please enter valid {0}")]
         //[Range(100001,699999,ErrorMessage ="{0} range between {1} to {2}")]
         public Double? PostalCode { get; set;}
 
+
+        public DateTime? FromDate { get; set; }
+
+        [DateRangeValidatorAttribute("FromDate",ErrorMessage="'FromDate' Should be older than or equel to 'ToDate'")]
+        public DateTime? ToDate { get; set; }
+
         
         public string? Password { get; set; }
 
         [Compare("Password", ErrorMessage="{0} cant match the {1}")]
-        public double? ConfPassword { get; set; }
+        public string? ConfPassword { get; set; }
 
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if(String.IsNullOrWhiteSpace(DateOfBirth.ToString()) && Age.HasValue == false)
+            {
+                yield return new ValidationResult("Please enter either Dateofbirth or Age", new[] {nameof(Age)});
+            }
+        }
     }
 }
